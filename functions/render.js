901a6190aecd6e0e82eda614c4793a2449353408
keniem16 <pradesoctavi@gmail.com>
2023-11-04 17:16:09 +0100

    1.1

--- functions/render.js
+++ functions/render.js
@@ -5,8 +5,27 @@ function render(dt){
     ctx.fillStyle = `rgb(50,50,50)`;
     ctx.fillRect(0, 0, canvas.width, canvas.height);
 
+    // DRAW DRONE
+
+    ctx.translate( worldToScreenX(drone.position.x), worldToScreenY(drone.position.y) );
+    ctx.rotate(-drone.angle * Math.PI / 180);
+    ctx.translate(- worldToScreenX(drone.position.x),- worldToScreenY(drone.position.y) );
+
     ctx.fillStyle = `rgb(255,50,50)`;
     ctx.fillRect(worldToScreenX(drone.position.x - 50), worldToScreenY(drone.position.y - 15), 100, 30);
+
+    ctx.translate( worldToScreenX(drone.position.x), worldToScreenY(drone.position.y) );
+    ctx.rotate(drone.angle * Math.PI / 180);
+    ctx.translate(- worldToScreenX(drone.position.x),- worldToScreenY(drone.position.y) );
+
+
+    ctx.fillStyle = `rgb(50,255,50)`;
+    ctx.fillRect(worldToScreenX(drone.position.x) - 3, worldToScreenY(drone.position.y) - 3, 6, 6);
+
+    for(let i = 0; i<4; i++){
+        ctx.fillRect(worldToScreenX(drone.vertex[i].x) - 3, worldToScreenY(drone.vertex[i].y) - 3, 6, 6);
+    }
+
 
     ctx.font = "20px serif";
     ctx.fillStyle = "#eee";
--- main.js
+++ main.js
@@ -1,10 +1,20 @@
 let camera = new Vec2(0, 0);
-let drone = 
-    {
-        position: new Vec2(0,0),
-        velocity: new Vec2(0,0)
-    }
+let drone = {
+    position: new Vec2(0,0),
+    velocity: new Vec2(0,0),
+    angle: 0,
+    vertex: [
+        new Vec2(0 - 50, 0 - 15),
+        new Vec2(0 + 50, 0 - 15),
+        new Vec2(0 - 50, 0 + 15),
+        new Vec2(0 + 50, 0 + 15)
+    ]
+};
 
+let ground = {
+    position: new Vec2(-1920, 1080),
+    size: new Vec2(1920 * 2, 50)
+}
 
 
 let lastUpdate = Date.now();
