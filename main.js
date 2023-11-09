bd177005cff48dcd2d9b152c51e9a6603b5b1706
keniem16 <141420294+keniem16@users.noreply.github.com>
2023-11-09 13:40:11 +0100

    testChromebook

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ let lastUpdate = Date.now();
 let tickInterval = setInterval(tick, 1000 / 60);
 
 function tick(){
-
+    //testChromebook
     let now = Date.now();
     let dt = (now - lastUpdate) / (1000 / 60);
     lastUpdate = now;
