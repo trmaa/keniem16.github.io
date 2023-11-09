08e4030d126396fe2f3578129704079b03832a49
keniem16 <141420294+keniem16@users.noreply.github.com>
2023-11-09 13:50:24 +0100

    chromebook

--- functions/render.js
+++ functions/render.js
@@ -2,7 +2,7 @@ const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext('2d');
 
 function render(dt){
-    ctx.fillStyle = `rgb(255,210,210)`;
+    ctx.fillStyle = `rgb(200,210,255)`;
     ctx.fillRect(0, 0, canvas.width, canvas.height);
 
     // DRAW GROUND
