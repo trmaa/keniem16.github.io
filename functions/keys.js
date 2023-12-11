3f3e6cf3fda514f71855e4ab93d1567562c18cbb
Pablo Marín <144141989+trmaa@users.noreply.github.com>
2023-12-11 18:11:16 +0100

    Update keys.js

--- functions/keys.js
+++ functions/keys.js
@@ -3,19 +3,19 @@ let leftDown = false;
 let rightDown = false;
 
 window.addEventListener("keydown", (event) => {
-    if(event.key == "ArrowLeft"){
+    if(event.key == "ArrowRight"){
         leftDown = true;
     }
-    else if(event.key == "ArrowRight"){
+    else if(event.key == "ArrowLeft"){
         rightDown = true;
     }
 });
 
 window.addEventListener("keyup", (event) => {
-    if(event.key == "ArrowLeft"){
+    if(event.key == "ArrowRight"){
         leftDown = false;
     }
-    else if(event.key == "ArrowRight"){
+    else if(event.key == "ArrowLeft"){
         rightDown = false;
     }
 });
